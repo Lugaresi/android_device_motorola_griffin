d79e925b401d492c5210414c651ff139c5a91cd0
Lugaresi <root@stukamanhpt4.com>
2020-07-26 05:21:44 +0200

    ACIPify a bit more: add e-mail to rom.

--- device.mk
+++ device.mk
@@ -451,3 +451,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
+
+# Email not in AICP?!
+PRODUCT_PACKAGES += \
+    Email
